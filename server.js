3e387b99963ed58ead0819b1c1003ecfff2c1640
MSoto3018 <sotomayormatias318@gmail.com>
2024-09-17 22:19:16 -0300

    implementacion de archivos de server

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const servidor = http.createServer((req, res) =>{
+    const parsedUrl = url.parse(req.url, true);
+        if(req.method == "GET" && parsedUrl.pathname == "/agregar-alumnos"){
+            // Mostrar formulario para agregar registro
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.readFile('TrabajoPractico.html', (err, data) => {
+                if (err) {
+                    res.writeHead(500);
+                    return res.end('Error cargando el formulario.');
+                    }
+                res.end(data);
+            });
+        }
+    });
+
+servidor.listen(3000, () => {
+console.log("Servidor ejecutándose");
+});
